7c93b9470fe24a810b313700080c416c9c628da0
Robin <robinsaxena.1999@gmail.com>
2020-04-27 14:53:41 +0530

    crawler added

---
+++ crawler/crawler/spiders/wikiSpider.py
@@ -0,0 +1,22 @@
+import scrapy
+class wikipediaSpider(scrapy.Spider):
+    name = "wikispider"
+    start_urls = [
+        'https://en.wikipedia.org/wiki/Information_retrieval'
+    ]
+
+    def parse(self,response):
+        page = response.url.split('/')[-1]
+        filename = 'webdoc-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+
+        sel = scrapy.Selector(response)
+        see_all_links = sel.xpath("//div[@class='div-col columns column-width']/ul/li/a/@href").extract()
+
+        for link in see_all_links:
+            if link is not None:
+                link = response.urljoin(link)
+                yield scrapy.Request(link,callback=self.parse)
+
+
